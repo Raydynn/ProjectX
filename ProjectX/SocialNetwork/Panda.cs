f42f0d58fb71084686902e40994449f36b85fb3f
Raydynn <Radin.todorov@abv.bg>
2016-01-21 20:29:52 +0200

    Added class Panda

---
+++ ProjectX/SocialNetwork/Panda.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork
+{
+    public enum GenderType { Male, Female };
+    public class Panda
+    {
+        private string email;
+        public string Name { get; set; }
+        public string Email
+        {
+            get { return email; }
+            set 
+            {
+                if (value.Contains("@")&&value.Substring(value.IndexOf('@')).Contains("."))
+                {
+                    email = value;
+                }            
+            }
+        }
+        public GenderType Gender { get; set; }
+        public bool IsMale { get; set; }
+        public bool IsFemale { get; set; }
+        public Panda(string name, string email, GenderType g)
+        {
+            Name = name;
+            this.Email = email;
+            Gender = g;
+            if (g == GenderType.Male)
+            {
+                IsMale = true;
+                IsFemale = false;
+            }
+            else
+            {
+                IsMale = false;
+                IsFemale = true;
+            }
+        }
+    }
+}
